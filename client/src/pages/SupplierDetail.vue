a48fbab718713acdf997657d0d7f6baff3ef8c6e
Steff Beckers <steff@steffbeckers.eu>
2018-05-21 18:34:24 +0200

    Supplier detail retrieve

--- client/src/pages/SupplierDetail.vue
+++ client/src/pages/SupplierDetail.vue
@@ -1,23 +1,45 @@
 <template>
   <v-container fluid>
     <v-layout column>
-      <div class="title">Leverancier</div>
+      <div class="title">{{ supplier.name }}</div>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        supplier: {}
-      }
-    },
-    watch: {
-      '$route' (to, from) {
-        console.log(to, from)
-      }
-    },
-    name: 'SupplierDetail'
-  }
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      loading: false,
+      supplier: {}
+    }
+  },
+  mounted: function () {
+    this.getSupplier()
+  },
+  methods: {
+    getSupplier () {
+      this.loading = true
+      axios.get(process.env.API + '/suppliers/slug/' + this.$route.params.slug)
+        .then(response => {
+          this.loading = false
+
+          this.supplier = response.data
+          console.log(this.supplier)
+        })
+        .catch(error => {
+          this.loading = false
+
+          console.error(error)
+        })
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.getSupplier()
+    }
+  },
+  name: 'SupplierDetail'
+}
 </script>
--- server/models/supplier.js
+++ server/models/supplier.js
@@ -4,4 +4,19 @@ module.exports = function(Supplier) {
   // Validations
   Supplier.validatesUniquenessOf('name', {message: 'is already a supplier'});
   Supplier.validatesUniquenessOf('slug', {message: 'is already taken by another supplier'});
+
+  // Find by slug
+  Supplier.bySlug = function(slug, cb) {
+    Supplier.findOne({where: {slug: slug}, include: 'menuItems'}, function(err, supplier) {
+      cb(null, supplier);
+    });
+  };
+  Supplier.remoteMethod(
+    'bySlug',
+    {
+      http: {path: '/slug/:slug', verb: 'get'},
+      accepts: {arg: 'slug', type: 'string'},
+      returns: {type: 'object', root: true},
+    }
+  );
 };
