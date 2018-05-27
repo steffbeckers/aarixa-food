ac4112f6267363790c2410d86c48a6240036c7a6
Steff Beckers <steff@steffbeckers.eu>
2018-05-27 21:40:21 +0200

    Disable user zoom on webpage (meta tag)

--- DEV.bat
+++ DEV.bat
@@ -3,6 +3,4 @@ start powershell -noexit -command "mongod"
 start powershell -noexit -command "npm start"
 start powershell -noexit -command "cd client; npm start"
 code C:\dev\aariXaFood
-start "" http://localhost:3333/
-start "" http://localhost:8080/
 exit
